ccdd72d58badf4b81d19634e8dac70ab221714af
yishaiz <yishai5555@gmail.com>
2016-09-02 12:34:14 +0300

    temp

--- src/components/todo-item/todo-item.component.ts
+++ src/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'aah-todo-item',
@@ -6,7 +6,76 @@ import { Component } from '@angular/core';
 
   template: `
 
+      <li *ngFor="let item of todoList" 
+        [ngClass]="getItemClass(item)">
+        <!--[ngClass]="{bold : item.isBold}"-->
+        
+        <div class="view">
+            <input class="toggle"
+                   type="checkbox"
+                   [checked] = "item.completed"
+                   
+                   #completedCheckbox
+                   (change) = "changeCompleted(item, completedCheckbox.checked)">
+                   
+                   <!--(change) = "changeCompleted(item)">-->
+                                      
+                       
+            <label (click)="editItem(item)">{{ item.title }}</label>
+            
+            <button class="destroy"
+                    (click) = "destroyItem(item)">
+            </button>
+          </div>
+          <input class="edit"
+                 #editValue
+                 [value]="item.editedTitle"
+                 (keyup.enter)="saveChange(item, editValue)"
+                 (keyup.escape)="undoChange(item)">
+
+      </li>
+
   `
 })
 
-export class TodoItemComponent {}
+export class TodoItemComponent {
+
+  @Input  item
+
+  getItemClass(item: any): any {
+    return {
+      completed: item.completed,
+      editing: item.editing
+    };
+  }
+
+  changeCompleted(item: any, checked: boolean): void {
+    // item.completed = !item.completed;
+    item.completed = checked;
+  }
+
+
+  changeCompletedOtherWay(item: any): void {
+    item.completed = !item.completed;
+  }
+
+
+
+
+  editItem(item: any): void {
+    item.editing = true;
+    item.editedTitle = item.title;
+  }
+
+  saveChange(item: any, editValue: any): void {
+    console.log('saveChange');
+
+    item.title = editValue.value;
+    item.editing = false;
+  }
+
+  undoChange(item: any): void {
+    console.log('undoChange');
+    item.editing = false;
+  }
+}
--- src/components/todo-list/todo-list.component.ts
+++ src/components/todo-list/todo-list.component.ts
@@ -13,37 +13,11 @@ import {Component} from '@angular/core';
   {{item.title}}
 </li>-->
 
+       <aah-todo-list *ngFor="let item of todoList"
+                      [item] ="item"> </aah-todo-list>  
+     
 
-   <!-- Item -->
-      <li *ngFor="let item of todoList" 
-        [ngClass]="getItemClass(item)">
-        <!--[ngClass]="{bold : item.isBold}"-->
-        
-        <div class="view">
-            <input class="toggle"
-                   type="checkbox"
-                   [checked] = "item.completed"
-                   
-                   #completedCheckbox
-                   (change) = "changeCompleted(item, completedCheckbox.checked)">
-                   
-                   <!--(change) = "changeCompleted(item)">-->
-                                      
-                       
-            <label (click)="editItem(item)">{{ item.title }}</label>
-            
-            <button class="destroy"
-                    (click) = "destroyItem(item)">
-            </button>
-          </div>
-          <input class="edit"
-                 #editValue
-                 [value]="item.editedTitle"
-                 (keyup.enter)="saveChange(item, editValue)"
-                 (keyup.escape)="undoChange(item)">
-
-      </li>
-
+   
 
  
     </ul>
@@ -60,46 +34,10 @@ export class TodoListComponent {
   ];
 
 
-  getItemClass(item: any): any {
-    return {
-      completed: item.completed,
-      editing: item.editing
-    };
-  }
-
-  changeCompleted(item: any, checked: boolean): void {
-    // item.completed = !item.completed;
-    item.completed = checked;
-  }
-
-
-  changeCompletedOtherWay(item: any): void {
-    item.completed = !item.completed;
-  }
-
   destroyItem(item: any): void {
     const index = this.todoList.indexOf(item);
 
     this.todoList.splice(index, 1);
   }
-
-
-  editItem(item: any): void {
-    item.editing = true;
-    item.editedTitle = item.title;
-  }
-
-  saveChange(item: any, editValue: any): void {
-    console.log('saveChange');
-
-    item.title = editValue.value;
-    item.editing = false;
-  }
-
-  undoChange(item: any): void {
-    console.log('undoChange');
-    item.editing = false;
-  }
-
 
 }
